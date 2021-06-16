6efc67df66f431d87448cb9f3107aa5726677a1c
huisheng.jin <huisheng.jinge@gmail.com>
2021-06-16 10:10:10 +0800

    init project and safety net

---
+++ src/test/java/com/gildedrose/TextTestFixture.java
@@ -0,0 +1,45 @@
+package com.gildedrose;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+public class TextTestFixture {
+    public static void main(String[] args) {
+        System.out.println(generateBaselineOutput());
+    }
+
+    public static String generateBaselineOutput() {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(outputStream);
+
+        printStream.println("OMGHAI!");
+
+        Item[] items = new Item[] {
+                new Item("+5 Dexterity Vest", 10, 20), //
+                new Item("Aged Brie", 2, 0), //
+                new Item("Elixir of the Mongoose", 5, 7), //
+                new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
+                new Item("Sulfuras, Hand of Ragnaros", -1, 80),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 1, 20),
+                // this conjured item does not work properly yet
+                new Item("Conjured Mana Cake", 3, 6) };
+
+        GildedRose app = new GildedRose(items);
+
+        for (int i = 0; i < 3; i++) {
+            printStream.println("-------- day " + i + " --------");
+            printStream.println("name, sellIn, quality");
+            for (Item item : items) {
+                printStream.println(item);
+            }
+            printStream.println();
+            app.update_quality();
+        }
+
+        return outputStream.toString();
+    }
+
+}
